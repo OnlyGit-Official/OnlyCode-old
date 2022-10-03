de9a62b6f8e1beedf1690575879cc408ab2e295c
Hardwaregore <hardwaregore101@gmail.com>
2022-10-03 19:50:42 +0000

    YAYAYAYAYAYAYAYAYAYAY IT STILL WURKS

--- webUI/repoconts.php
+++ webUI/repoconts.php
@@ -3,16 +3,21 @@ $contents = $_GET['path'];
 
 
 
-$conts = shell_exec("cat $contents");
+// $conts = shell_exec("cat $contents");
+
+$fs = filesize("$contents");
+$f = fopen($contents, 'r');
+$conts = fread($f, $fs);
+fclose($f);
 
 if (is_dir($contents)) {
   header("Location: folderinrepo.php?folder=$contents");
 } else {
   echo "--FILENAME:$contents--";
   echo "<br>";
-  echo "--------------------------------------";
+  echo "-----------------------------------------------";
   echo "<br>";
-  echo "$conts";
+  echo nl2br($conts);
 }
 
 
