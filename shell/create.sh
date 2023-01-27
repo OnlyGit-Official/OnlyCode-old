0c3b92fed75756b51ab565b8e58d43f1caf2c9b6
Hardwaregore <hardwaregore101@gmail.com>
2023-01-27 14:54:33 -0600

    instructions

--- shell/create.sh
+++ shell/create.sh
@@ -4,6 +4,9 @@ mkdir -p git/$1
 git init --bare git/$1
 cd git/$1
 touch README.md
+echo "
+# This is a bare repo. Please clone this repo on your dev env to make changes.
+" >> README.md
 git add .
 git config --global user.name "SelfHostedGIT"
 git config --global user.email "Self@hosted.git"
